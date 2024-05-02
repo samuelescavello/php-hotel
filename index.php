e1b2c1ba2fd95284c482ce3dcffd8fdcde8b9ab2
samuelescavello <scavellosamuele23@gmail.com>
2024-05-02 19:02:25 +0200

    finish

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-    include  __DIR__ . "/views/header.php";
     include __DIR__ ."/models/hotel.php";
-    if(!empty($_GET['search']) || isset($_GET['search']) && $GET_['search'] == 0){
+    if(!empty($_GET['search']) || isset($_GET['search']) && $_GET['search'] == 0){
         $search = $_GET['search'];
         $newhotel = array_filter($hotels, function($hotel) use ($search){
-            return $hotel ['parking'] == $search || $hotel == "all";
+            return $hotel ['parking'] == $search || $search == "all";
         });
     }else{
         $newhotel = $hotels;
     }
-    
+    include  __DIR__ . "/views/header.php";
 ?>
 <main class="container">
     <?php
